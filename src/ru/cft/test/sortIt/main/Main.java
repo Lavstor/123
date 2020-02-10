e32c5124319f78d6f6f2f022d081ccc0b730a1ca
Lavstor <45934398+Lavstor@users.noreply.github.com>
2020-02-10 13:54:31 +0300

    123

--- src/ru/cft/test/sortIt/main/Main.java
+++ src/ru/cft/test/sortIt/main/Main.java
@@ -3,8 +3,7 @@ package ru.cft.test.sortIt.main;
 import ru.cft.test.sortIt.sort.Sort;
 
 public class Main {
-    public static void main(String[] args) {
-        new Sort(args).sort();
-
+    public static void main(String[] args, String mode) {
+        new Sort(args[1], args[2]).sort();
     }
 }
--- src/ru/cft/test/sortIt/sort/Sort.java
+++ src/ru/cft/test/sortIt/sort/Sort.java
@@ -4,10 +4,13 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-public class Sort {
+public class Sort<T> {
+    private final String[] filePassArray;
     private ArrayList<Scanner> scanners;
+    private Comparator<T> comparator;
 
-    public Sort(String[] passArray) {
+    public Sort(String[] passArray, String mode) {
+        filePassArray = passArray;
         scanners = new ArrayList<>();
 
         for (String s : passArray) {
@@ -17,37 +20,74 @@ public class Sort {
                 e.printStackTrace();
             }
         }
+
+        createComparator(mode);
     }
 
     public void sort() {
-        List<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        scanners.forEach(e -> list.add(e.nextInt()));
 
-        for (int i = 0; i < scanners.size(); i++) {
-            list.add( readNext(i));
-        }
+        T data;
+        T lastData = null;
+        int index;
 
-        int indexDelete = 0;
         while (!list.isEmpty()) {
-            int nextData = list.get(indexDelete);
+            index = 0;
+            //noinspection unchecked
+            data = (T) list.get(0);
 
             for (int i = 0; i < list.size(); i++) {
-                if (nextData < list.get(i)) {
-                    nextData = list.get(i);
-                    indexDelete = i;
+                //noinspection unchecked
+                if (comparator.compare((T) list.get(i), data) < 0) {
+                    //noinspection unchecked
+                    data = (T) list.get(i);
+                    index = i;
                 }
             }
 
-            if (scanners.get(indexDelete).hasNext()) {
-                list.set(indexDelete, readNext(indexDelete));
-            } else{
-                list.remove(indexDelete);
+            if (comparator.compare(lastData, data) <= 0) {
+                System.out.println(data);
+                lastData = data;
+            } else {
+                System.out.println("Ошибка! Нарушен порядок сортировки в документе: " + filePassArray[index] +
+                        "\n Элемент: " + data + " - пропущен");
             }
 
-            System.out.println(nextData);
+            if (scanners.get(index).hasNext()) {
+                list.set(index, scanners.get(index).nextInt());
+            } else {
+                scanners.remove(index);
+                list.remove(index);
+            }
         }
     }
 
-    private int readNext(int index) {
-        return scanners.get(index).nextInt();
+    private void createComparator(String mode) {
+        this.comparator = (o1, o2) -> {
+            if (o1 == null && o2 == null) {
+                return 0;
+            }
+
+            if (o1 == null) {
+                return -1;
+            }
+
+            if (o2 == null) {
+                return 1;
+            }
+
+            if (mode.equals("-d")) {
+                @SuppressWarnings("unchecked")
+                Comparable<T> object2 = (Comparable<T>) o2;
+
+                return object2.compareTo(o1);
+            }
+
+            @SuppressWarnings("unchecked")
+            Comparable<T> object1 = (Comparable<T>) o1;
+
+            return object1.compareTo(o2);
+        };
     }
 }
