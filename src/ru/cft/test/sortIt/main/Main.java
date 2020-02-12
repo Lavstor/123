acbc8932677e03f446a86638bc9e6fcf46119d90
Lavstor <45934398+Lavstor@users.noreply.github.com>
2020-02-12 07:53:36 +0300

    Добавил исключение, если нехватка аргументов

--- src/ru/cft/test/sortIt/main/Main.java
+++ src/ru/cft/test/sortIt/main/Main.java
@@ -6,10 +6,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        if (args[0].equals("-d")) {
+
+        if (args.length > 3 && args[0].equals("-d")) {
             new Sort(args[0], args[1], args[2], Arrays.copyOfRange(args, 3, args.length)).sort();
-        } else {
+        } else if (args.length > 2) {
             new Sort("-a", args[0], args[1], Arrays.copyOfRange(args, 2, args.length)).sort();
+        } else {
+            throw new IllegalArgumentException("Убедитесь что были введены все аргументы!");
         }
     }
 }
